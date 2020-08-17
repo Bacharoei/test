2b6f638f4e7272d3faca0185283ba80ae6996491
Roei Oz Bachar <roei.ob@zerto.com>
2020-08-17 11:58:48 +0300

    changes

--- Server.js
+++ Server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./App");
-const port = process.env.PORT || 3030 ;
+const port = process.env.PORT  ;
 
 const server = http.createServer(app);
 server.listen(port);
